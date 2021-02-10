0bc95748038fdc3baa167a9aea902be3a4372139
jensrijks <jens.rijks@student.hu.nl>
2021-02-10 23:10:13 +0100

    Second commit

---
+++ src/JavaTutorial/Main.java
@@ -0,0 +1,14 @@
+package JavaTutorial;
+
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String message = "Hello world" + "!!";
+
+        System.out.println(message);
+    }
+
+}
