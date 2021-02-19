ea0c0a921bc1a79d8b84248e956ca008d6c9f4a2
jensrijks <jens.rijks@student.hu.nl>
2021-02-19 17:03:18 +0100

    commit 19-02 Practicum 4A

---
+++ src/OpdrachtenLes/Opdracht5_3/Main.java
@@ -0,0 +1,14 @@
+package OpdrachtenLes.Opdracht5_3;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> huisdieren = new ArrayList<String>();
+
+
+    }
+
+}
---
+++ src/Practica/Practicum4A/Huis.java
@@ -0,0 +1,22 @@
+package Practica.Practicum4A;
+
+public class Huis {
+
+    private String adres;
+    private int bouwjaar;
+    private Persoon huisbaas;
+
+    public Huis(String adr, int bwjr) {
+        adres = adr;
+        bouwjaar = bwjr;
+    }
+
+    public void setHuisbaas(Persoon hb) {huisbaas = hb; }
+
+    public Persoon getHuisbaas() {return huisbaas; }
+
+    public String toString() {
+        String s = "Huis " + adres + " is gebouwd in " + bouwjaar + "\n" + "en heeft huisbaas " + huisbaas;
+        return s;
+    }
+}
